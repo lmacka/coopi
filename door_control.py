a71fda15a66b111d77f926887f274506b7a37f12
Liam <lmacka@gmail.com>
2024-05-16 20:07:33 +1000

    Fixed relays and added dep

--- door_control.py
+++ door_control.py
@@ -55,8 +55,8 @@ def close_door():
 def delayed_open():
     if automationhat.is_automation_hat():
         automationhat.light.comms.on()
-        automationhat.relay.one.off()
-        automationhat.relay.two.on()
+        automationhat.relay.one.on()
+        automationhat.relay.two.off()
         time.sleep(actuateTime)
         automationhat.light.comms.off()
     # Release the lock
@@ -65,8 +65,8 @@ def delayed_open():
 def delayed_close():
     if automationhat.is_automation_hat():
         automationhat.light.comms.on()
-        automationhat.relay.one.on()
-        automationhat.relay.two.off()
+        automationhat.relay.one.off()
+        automationhat.relay.two.on()
         time.sleep(actuateTime)
         automationhat.light.comms.off()
     # Release the lock
