d3edc8af5648e84cb0b1b0c64a1da06479c3cbf8
Liam <lmacka@gmail.com>
2024-05-16 14:42:02 +1000

    Working prototype

--- app.py
+++ app.py
@@ -1,1 +1,44 @@
-stream.py
+from flask import Flask, render_template, request, jsonify, Response
+import subprocess
+import logging
+import io
+import door_control
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/open_door', methods=['POST'])
+def open_door_route():
+    result = door_control.open_door()
+    return jsonify({'message': result})
+
+@app.route('/close_door', methods=['POST'])
+def close_door_route():
+    result = door_control.close_door()
+    return jsonify({'message': result})
+
+@app.route('/lock_status', methods=['GET'])
+def lock_status_route():
+    result = door_control.get_lock_status()
+    return jsonify({'lock_engaged': result})
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def gen():
+    command = ['libcamera-vid', '-t', '0', '-o', '-']
+    ffmpeg_command = ['ffmpeg', '-i', '-', '-f', 'mjpeg', '-']
+    camera_process = subprocess.Popen(command, stdout=subprocess.PIPE)
+    ffmpeg_process = subprocess.Popen(ffmpeg_command, stdin=camera_process.stdout, stdout=subprocess.PIPE)
+    while True:
+        frame = ffmpeg_process.stdout.read(1024)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
---
+++ door_control.py
@@ -0,0 +1,100 @@
+import sys
+import RPi.GPIO as GPIO
+import automationhat
+import threading
+import json
+import time
+
+############ CONFIG ###############
+statefile = 'static/state.json'
+actuateTime = 10
+# actuateTime = 90
+###################################
+
+# Create a lock
+lock = threading.Lock()
+
+def get_lock_status():
+    return lock.locked()
+
+def open_door():
+    # Check if the lock is acquired
+    if lock.locked():
+        return "Another operation is in progress"
+
+    with open(statefile) as json_file:
+        json_data = json.load(json_file)
+        doorstate = json_data['state']
+    if ( doorstate == 'closed' ):
+        with open(statefile, 'w') as f:
+            json.dump({'state': 'open'}, f, ensure_ascii=False)
+        # Acquire the lock and start a new thread
+        lock.acquire()
+        threading.Thread(target=delayed_open).start()
+        return 'Opening door. This will take ' + str(actuateTime) + ' seconds'
+    else:
+        return "Door already open"
+
+def close_door():
+    # Check if the lock is acquired
+    if lock.locked():
+        return "Another operation is in progress"
+
+    with open(statefile) as json_file:
+        json_data = json.load(json_file)
+        doorstate = json_data['state']
+    if ( doorstate == 'open' ):
+        with open(statefile, 'w') as f:
+            json.dump({'state': 'closed'}, f, ensure_ascii=False)
+        # Acquire the lock and start a new thread
+        lock.acquire()
+        threading.Thread(target=delayed_close).start()
+        return 'Closing door. This will take ' + str(actuateTime) + ' seconds'
+    else:
+        return "Door already closed"
+
+def delayed_open():
+    # if automationhat.is_automation_hat():
+    #     automationhat.light.power.write(1) # Turn on the power LED
+    #     automationhat.light.comms.on()
+    #     automationhat.relay.one.off()
+    #     automationhat.relay.two.on()
+    time.sleep(actuateTime)
+    #     automationhat.light.comms.off()
+    # Release the lock
+    lock.release()
+
+def delayed_close():
+    # if automationhat.is_automation_hat():
+    #     automationhat.light.power.write(1) # Turn on the power LED
+    #     automationhat.light.comms.on()
+    #     automationhat.relay.one.on()
+    #     automationhat.relay.two.off()
+    time.sleep(actuateTime)
+    #     automationhat.light.comms.off()
+    # Release the lock
+    lock.release()
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Script to open and close the door, for use with cron")
+        print("Usage: python door_control.py <command>")
+        print("<command> can be 'open' or 'close'")
+        sys.exit(1)
+
+    command = sys.argv[1]
+    if command == 'open':
+        print(open_door())
+    elif command == 'close':
+        print(close_door())
+    else:
+        print(f"Invalid command: {command}")
+        sys.exit(1)
+
+    command = sys.argv[1]
+    if command == 'open':
+        open_door()
+    elif command == 'close':
+        close_door()
+    else:
+        print(f"Invalid command: {command}")
+        sys.exit(1)
