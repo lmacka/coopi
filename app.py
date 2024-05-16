c9bc011aba12571836925269921bcc6ce76d9602
Liam <lmacka@gmail.com>
2024-05-16 19:45:37 +1000

    Add video streaming

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request, jsonify, Response
-import subprocess
-import logging
-import io
+from flask import Flask, render_template, jsonify
+import automationhat
 import door_control
+import atexit
 
 app = Flask(__name__)
+
+if automationhat.is_automation_hat():
+    atexit.register(automationhat.light.power.off)
 
 @app.route('/')
 def home():
@@ -25,20 +27,7 @@ def lock_status_route():
     result = door_control.get_lock_status()
     return jsonify({'lock_engaged': result})
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-def gen():
-    command = ['libcamera-vid', '-t', '0', '-o', '-']
-    ffmpeg_command = ['ffmpeg', '-i', '-', '-f', 'mjpeg', '-']
-    camera_process = subprocess.Popen(command, stdout=subprocess.PIPE)
-    ffmpeg_process = subprocess.Popen(ffmpeg_command, stdin=camera_process.stdout, stdout=subprocess.PIPE)
-    while True:
-        frame = ffmpeg_process.stdout.read(1024)
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
 if __name__ == "__main__":
+    if automationhat.is_automation_hat():
+        automationhat.light.power.on()
     app.run(host='0.0.0.0', debug=True)
